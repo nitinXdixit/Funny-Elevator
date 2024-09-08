e746ec7f4139c240896e57543dea78f52971e5ea
Nitin Dixit <13nitindixit@gmail.com>
2024-09-08 10:05:29 +0530

    Modify: Header, footer style and input range slider

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
- 
+// input handling
+const floorSlider = document.getElementById("floorSlider");
+const elevatorSlider = document.getElementById("elevatorSlider");
+
+const floorValue = document.getElementById("floorValue");
+const elevatorValue = document.getElementById("elevatorValue");
+
+floorSlider.addEventListener("input", (event) => {
+  floorValue.textContent = event.target.value;
+});
+
+elevatorSlider.addEventListener("input", (event) => {
+  elevatorValue.textContent = event.target.value;
+});
+
+//elevators UI
+const elevatorUI = document.getElementById("elevatorsUI");
+
+let elevators = null;
+let num;
